6baa39bb6cc083d0874d411dee18fa477b248e71
baranov-V-V <baranov.vv@phystech.edu>
2023-05-30 14:38:51 +0300

    added basic service

---
+++ config/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+struct Config {
+  static const std::string host = "host";
+  static const std::string port = "1234";
+  static const std::string login = "root";
+  static const std::string password = "root";
+  static const std::string database = "mysql";
+};
